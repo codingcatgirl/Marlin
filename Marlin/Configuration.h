6feac9586366f910f536fcd4dbcb67b4b9d166f2
Laura Klünder <laura@codingcatgirl.de>
2021-01-22 15:14:34 +0100

    adjust min and max positions

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1132,12 +1132,12 @@
 #define Y_BED_SIZE 230
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -22
-#define Y_MIN_POS -22
+#define X_MIN_POS -24
+#define Y_MIN_POS -23
 #define Z_MIN_POS 0
 #define X_MAX_POS 259-5
 #define Y_MAX_POS 236-5
-#define Z_MAX_POS 250
+#define Z_MAX_POS 240
 
 /**
  * Software Endstops
