13a5daa89d06488b5d8101f730fd4260d365af90
Laura Klünder <laura@codingcatgirl.de>
2021-04-01 13:12:30 +0200

    bump config version

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Laura, Foxy Printer 2.0 config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Laura, Foxy Printer 2.1 config)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
